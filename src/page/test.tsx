ed44134726c23a6f8a78f8968c6906455c1299ca
kimkh05 <nestjs01@naver.com>
2023-03-14 16:59:46 +0900

    fix: .

--- src/page/test.tsx
+++ src/page/test.tsx
@@ -1,13 +1,28 @@
 import { useState } from "react";
+import Button from "../components/common/button";
+import Input from "../components/common/input";
 
 const Test = () => {
   const [state, setState] = useState<number>(0);
+  const [value, setValue] = useState<string>("");
   const onClick = () => {
     setState(state + 1);
   };
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
   return (
     <>
-      <span onClick={onClick}>{state}</span>
+      <Button onClick={onClick} color="main01">
+        {state}
+      </Button>
+      <Input
+        text="아이디"
+        placeholder="아이디를 입력해주세요."
+        onChange={onChange}
+        value={value}
+      />
     </>
   );
 };
