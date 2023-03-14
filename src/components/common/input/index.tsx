1f29aa9e8baf8e71918217bb6acde7d6afa7f589
kimkh05 <nestjs01@naver.com>
2023-03-14 16:59:28 +0900

    feat: input textfield

---
+++ src/components/common/input/index.tsx
@@ -0,0 +1,79 @@
+import styled from "styled-components";
+import { useState, useRef } from "react";
+
+interface PropsType {
+  text: string;
+  type?: "text" | "password";
+  name?: string;
+  placeholder: string;
+  value?: string;
+  error?: boolean;
+  errorMsg?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input = ({
+  text,
+  type,
+  name,
+  placeholder,
+  value,
+  error,
+  errorMsg,
+  onChange,
+}: PropsType) => {
+  const [clickState, setClickState] = useState<boolean>(false);
+  const ref = useRef<HTMLInputElement>(null);
+  return (
+    <_Wrapper>
+      <_UpperText>{text}</_UpperText>
+      <_InputWrapper error={error} clickState={clickState}>
+        <_Input
+          ref={ref}
+          type={type}
+          name={name}
+          placeholder={clickState ? "" : placeholder}
+          value={value}
+          onChange={onChange}
+          autoComplete="off"
+          onFocus={() => setClickState(true)}
+          onBlur={() => setClickState(value ? true : false)}
+        />
+      </_InputWrapper>
+    </_Wrapper>
+  );
+};
+
+export default Input;
+
+const _Wrapper = styled.div`
+  width: 500px;
+`;
+
+const _UpperText = styled.p`
+  font-size: 12px;
+  color: ${({ theme }) => theme.color.main02};
+`;
+
+const _InputWrapper = styled.div<{ clickState: boolean; error?: boolean }>`
+  width: 100%;
+  padding: 20px 10px;
+  border-radius: 13px;
+  border: 1px solid
+    ${({ theme, clickState }) =>
+      clickState ? theme.color.main02 : theme.color.gray600};
+  :focus-within {
+    border: 1px solid
+      ${({ theme, error }) => (error ? theme.color.error : theme.color.main02)};
+  }
+`;
+
+const _Input = styled.input`
+  width: 100%;
+  background: transparent;
+  color: ${({ theme }) => theme.color.main02};
+  font-size: 18px;
+  ::placeholder {
+    color: ${({ theme }) => theme.color.gray600};
+  }
+`;
