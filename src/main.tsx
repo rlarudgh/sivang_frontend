cfb9e8f91e3814931db019754976429d85d76ce2
kimkh05 <nestjs01@naver.com>
2023-03-15 11:27:34 +0900

    feat: setting react-toastify

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,20 @@ import ReactDOM from "react-dom/client";
 import StyleProvider from "./styles";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { RecoilRoot } from "recoil";
-import Test from "./page/test";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import LoginPage from "./page/login";
+import MainPage from "./page/main";
 
 const App = () => {
   return (
     <StyleProvider>
       <RecoilRoot>
+        <ToastContainer />
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Test />} />
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/main" element={<MainPage />} />
           </Routes>
         </BrowserRouter>
       </RecoilRoot>
