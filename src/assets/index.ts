fa4883f435b83b4fbde4e9be12f7db887472a214
kimkh05 <nestjs01@naver.com>
2023-03-16 20:16:20 +0900

    feat: init images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,6 @@
 export { default as Logo } from "./image/logo.svg";
 export { default as LoginImage } from "./image/login_image.svg";
+export { default as MainBackground } from "./image/main_background.svg";
+export { default as Plus } from "./image/plus.svg";
+export { default as Minus } from "./image/minus.svg";
+export { default as Garbage } from "./image/garbage.svg";
