e8043ca7be1df14bcdf09b03687defd69352db29
kimkh05 <nestjs01@naver.com>
2023-03-20 19:08:43 +0900

    feat: payment type

--- src/types/modal/index.ts
+++ src/types/modal/index.ts
@@ -1,3 +1,11 @@
 export interface ModalType {
   setModal: (modal: boolean) => void;
 }
+
+export interface PaymentType{
+    id: number;
+    type: "+" | "-";
+    title: string;
+    content: string;
+    cost: number;
+}
