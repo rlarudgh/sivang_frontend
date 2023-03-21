60e26df1472aa3b61b68ab73858b8bb1ed9d5017
kimkh05 <nestjs01@naver.com>
2023-03-21 16:47:59 +0900

    feat: setting react-query

--- src/main.tsx
+++ src/main.tsx
@@ -6,19 +6,36 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import LoginPage from "./page/login";
 import MainPage from "./page/main";
+import WritePage from "./page/write";
+import CyclePage from "./page/cycle";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      keepPreviousData: true,
+      refetchOnWindowFocus: false,
+      staleTime: 5000,
+    },
+  },
+});
 
 const App = () => {
   return (
     <StyleProvider>
-      <RecoilRoot>
-        <ToastContainer />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/main" element={<MainPage />} />
-          </Routes>
-        </BrowserRouter>
-      </RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <ToastContainer />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<LoginPage />} />
+              <Route path="/main" element={<MainPage />} />
+              <Route path="/write" element={<WritePage />} />
+              <Route path="/cycle" element={<CyclePage />} />
+            </Routes>
+          </BrowserRouter>
+        </RecoilRoot>
+      </QueryClientProvider>
     </StyleProvider>
   );
 };
