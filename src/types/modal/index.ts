456140a9d6e0b23d8aafd526d2f1acaa56bcc963
kimkh05 <nestjs01@naver.com>
2023-03-21 09:06:58 +0900

    style: prettier

--- src/types/modal/index.ts
+++ src/types/modal/index.ts
@@ -2,10 +2,10 @@ export interface ModalType {
   setModal: (modal: boolean) => void;
 }
 
-export interface PaymentType{
-    id: number;
-    type: "+" | "-";
-    title: string;
-    content: string;
-    cost: number;
+export interface PaymentType {
+  id: number;
+  type: "+" | "-";
+  title: string;
+  content: string;
+  cost: number;
 }
