add98fc19f022b7b1bbffbcd54c06c28e4d32abe
kimkh05 <nestjs01@naver.com>
2023-04-11 09:30:33 +0900

    feat: init page

---
+++ src/page/record/index.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import Header from "@/components/common/header";
+
+const RecordPage = () => {
+  return (
+    <>
+      <span>Record Page</span>
+    </>
+  );
+};
+
+export default RecordPage;
---
+++ src/page/regular/index.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+import Header from '@/components/common/header';
+
+const RegularPage = () => {
+  return (
+    <_Wrapper>
+      <Header />
+      <div>
+        
+      </div>
+    </_Wrapper>
+  );
+};
+
+export default RegularPage;
+
+const _Wrapper = styled.body`
+  width: 100vw;
+  height: 100vh;
+`;
