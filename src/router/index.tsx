1e0af8a5a6f618b1745a58bd260091891f935f54
kimkh05 <nestjs01@naver.com>
2023-04-11 09:13:21 +0900

    fix: absolute path

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,8 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Suspense } from 'react';
-import LoginPage from '../page/login';
-import MainPage from '../page/main';
-import WritePage from '../page/write';
+import LoginPage from '@/page/login';
+import MainPage from '@/page/main';
+import WritePage from '@/page/write';
+import MyPage from '@/page/mypage';
+import RegularPage from '@/page/regular';
+import RecordPage from '@/page/record';
 
 export const Router = () => {
   return (
@@ -13,6 +16,9 @@ export const Router = () => {
           <Route path="/" element={<LoginPage />} />
           <Route path="/main" element={<MainPage />} />
           <Route path="/write" element={<WritePage />} />
+          <Route path="/regular" element={<RegularPage />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/record" element={<RecordPage />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
