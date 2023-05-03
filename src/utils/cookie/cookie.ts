c681b663407c0fc7ffc09169edf7e100851d127f
kimkh05 <nestjs01@naver.com>
2023-05-03 14:58:16 +0900

    fix: fix cookie function

--- src/utils/cookie/cookie.ts
+++ src/utils/cookie/cookie.ts
@@ -6,12 +6,10 @@ export const getCookie = (name: string) => {
   return cookies.get(name);
 };
 
-export const setCookie = (name: string, value: string) => {
-  cookies.set(name, value, { path: '/' });
+export const setCookie = (name: string, value: string, option?: any) => {
+  cookies.set(name, value, { path: '/', ...option });
 };
 
 export const removeCookie = (name: string) => {
   cookies.remove(name, { path: '/' });
 };
-
-
