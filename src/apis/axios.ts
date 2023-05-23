273fcd25bdeb535dabbc87fd293bb9ea06589d29
kimkh05 <nestjs01@naver.com>
2023-05-23 09:27:22 +0900

    fix: change base-url

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: `${import.meta.env.REACT_APP_BASE_URL}`,
   timeout: 2000,
 });
 
--- src/apis/login.ts
+++ src/apis/login.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 import { LoginType } from '@/types/login';
 
 export const Login = async (data: LoginType) => {
-  const response = await axios.post('http://localhost:3001/auth/login', data);
+  const response = await axios.post(`${import.meta.env.VITE_APP_BASE_URL}/auth/login`, data);
   return response;
 };
