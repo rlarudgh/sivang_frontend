5ec1c70a3ca4401dcdfb9e56d783149a59e4bcaf
kimkh05 <nestjs01@naver.com>
2023-05-24 23:00:49 +0900

    fix: setting instance interceptors

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,8 +1,19 @@
-import axios from 'axios';
+import { getCookie } from '@/utils/cookie';
+import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 
 const instance = axios.create({
   baseURL: `${import.meta.env.REACT_APP_BASE_URL}`,
   timeout: 2000,
 });
+
+instance.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    config.headers.Authorization = `Bearer ${getCookie('accessToken')}`;
+    return config;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  },
+);
 
 export default instance;
