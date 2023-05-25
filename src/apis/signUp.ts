ec6f99ae1fbecf3e27e6732f76c014d57ff3f5b9
kimkh05 <nestjs01@naver.com>
2023-05-25 19:26:26 +0900

    fix: delete /

--- src/apis/signUp.ts
+++ src/apis/signUp.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 import { SignUpType } from '@/types/signUp';
 
 export const SignUp = async (data: SignUpType) => {
-  const response = await axios.post(`${import.meta.env.VITE_APP_BASE_URL}/auth/signUp`, data);
+  const response = await axios.post(`${import.meta.env.VITE_APP_BASE_URL}auth/signUp`, data);
   return response;
 };
