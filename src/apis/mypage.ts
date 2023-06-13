a9f907e96b2edaccc6b5de1d5d3d27c345b98257
kimkh05 <nestjs01@naver.com>
2023-06-13 15:10:26 +0900

    fix: mypage api type modify

--- src/apis/mypage.ts
+++ src/apis/mypage.ts
@@ -1,7 +1,6 @@
 import instance from './axios';
-import { MyPageListType } from '@/types/mypage';
 
 export const getProfile = async () => {
-  const response = await instance.get<MyPageListType>('/mypage/getProfile');
+  const response = await instance.get('/mypage/getProfile');
   return response;
 };
