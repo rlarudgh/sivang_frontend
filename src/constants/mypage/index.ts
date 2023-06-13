d8a483383d7078aadb2e67c6615624a5dcde5f7a
kimkh05 <nestjs01@naver.com>
2023-06-13 10:03:20 +0900

    fix: delete file

--- src/constants/mypage/index.ts
+++
@@ -1,9 +0,0 @@
-import { MyPageListType } from '../../types/mypage';
-
-export const MyPageList: MyPageListType = {
-  email: 'test1234@dsm.hs.kr',
-  join: '2023.01.01',
-  autoPost: 1,
-  totalUse: 10,
-  totalSave: 12,
-};
