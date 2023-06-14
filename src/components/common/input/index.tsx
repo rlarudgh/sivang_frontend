84de4a385528225fa4e63757f2b9f728a59310b2
kimkh05 <nestjs01@naver.com>
2023-06-14 11:18:08 +0900

    fix: onKeyPress

--- src/components/common/input/index.tsx
+++ src/components/common/input/index.tsx
@@ -1,27 +1,19 @@
-import styled from "styled-components";
-import { useState, useRef } from "react";
+import styled from 'styled-components';
+import { useState, useRef } from 'react';
 
 interface PropsType {
   text: string;
-  type?: "text" | "password";
+  type?: 'text' | 'password';
   name?: string;
   placeholder: string;
   value?: string;
   error?: boolean;
   errorMsg?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onKeyPress?(e: React.KeyboardEvent<HTMLInputElement>): void;
 }
 
-const Input = ({
-  text,
-  type,
-  name,
-  placeholder,
-  value,
-  error,
-  errorMsg,
-  onChange,
-}: PropsType) => {
+const Input = ({ text, type, name, placeholder, value, error, errorMsg, onChange, onKeyPress }: PropsType) => {
   const [clickState, setClickState] = useState<boolean>(false);
   const ref = useRef<HTMLInputElement>(null);
 
@@ -33,10 +25,11 @@ const Input = ({
           ref={ref}
           type={type}
           name={name}
-          placeholder={clickState ? "" : placeholder}
+          placeholder={clickState ? '' : placeholder}
           value={value}
           onChange={onChange}
           autoComplete="off"
+          onKeyPress={onKeyPress}
           onFocus={() => setClickState(true)}
           onBlur={() => setClickState(value ? true : false)}
         />
@@ -60,12 +53,9 @@ const _InputWrapper = styled.div<{ clickState: boolean; error?: boolean }>`
   width: 100%;
   padding: 20px 10px;
   border-radius: 13px;
-  border: 1px solid
-    ${({ theme, clickState }) =>
-      clickState ? theme.color.main02 : theme.color.gray600};
+  border: 1px solid ${({ theme, clickState }) => (clickState ? theme.color.main02 : theme.color.gray600)};
   :focus-within {
-    border: 1px solid
-      ${({ theme, error }) => (error ? theme.color.error : theme.color.main02)};
+    border: 1px solid ${({ theme, error }) => (error ? theme.color.error : theme.color.main02)};
   }
 `;
 
